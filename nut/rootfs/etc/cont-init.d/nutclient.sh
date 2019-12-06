d5dab554869bf353a34283b670f555ea56af73c6
Paul Sinclair <sinclairpaul@hotmail.com>
2019-12-06 16:28:18 -0500

    💥Rewrite config script

---
+++ nut/rootfs/etc/cont-init.d/nutclient.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: Network UPS Tools
+# Configures Network UPS Tools for Client Mode only
+# ==============================================================================
+declare -a CONF_ENTRIES=("name" "host" "password" "user")
+
+if bashio::config.equals 'mode' 'netclient' ;then
+
+    for entry in "${CONF_ENTRIES[@]}"; do
+        if ! bashio::config.exists "remote_ups_${entry}";then
+        bashio::log.fatal \
+		"Netclient mode specified but no ${entry} is configured"
+		bashio::exit.nok
+	    fi
+    done
+
+    rname=$(bashio::config "remote_ups_name")
+    rhost=$(bashio::config "remote_ups_host")
+    ruser=$(bashio::config "remote_ups_user")
+    rpwd=$(bashio::config "remote_ups_password")
+
+    echo "MONITOR ${rname}@${rhost} 1 ${ruser} ${rpwd} slave" \
+        >> /etc/nut/upsmon.conf
+fi
